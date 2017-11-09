33fab75d893d585d26a48821250b24073bda6c72
David Däster <david.daester@unic.com>
2017-11-09 17:56:05 +0100

    Remove DAO

--- src/main/kotlin/net/daester/david/flywayjooqexample/domain/AirportRepository.kt
+++ src/main/kotlin/net/daester/david/flywayjooqexample/domain/AirportRepository.kt
@@ -1,7 +1,8 @@
 package net.daester.david.flywayjooqexample.domain
-
 data class Airport(val id: Int, val ident: String, val type: String, val name: String, val scheduledService: Boolean?, val position: AirportPosition)
 data class AirportPosition(val longitudeDeg: Double?, val latitudeDeg: Double?)
+
+
 
 interface AirportRepository {
     fun getById(id: Int): Airport
--- src/main/kotlin/net/daester/david/flywayjooqexample/infrastructure/jooq/JooqAirportRepository.kt
+++ src/main/kotlin/net/daester/david/flywayjooqexample/infrastructure/jooq/JooqAirportRepository.kt
@@ -1,35 +1,30 @@
 package net.daester.david.flywayjooqexample.infrastructure.jooq
-
-import ch.zvv.kds.infrastructure.jooq.Tables
-import ch.zvv.kds.infrastructure.jooq.Tables.*
-import ch.zvv.kds.infrastructure.jooq.enums.AirportsScheduledService
-import ch.zvv.kds.infrastructure.jooq.enums.AirportsType
-import ch.zvv.kds.infrastructure.jooq.enums.VAirportDistancesTargetAirportScheduledService
-import ch.zvv.kds.infrastructure.jooq.tables.daos.AirportsDao
-import ch.zvv.kds.infrastructure.jooq.tables.pojos.Airports
-import ch.zvv.kds.infrastructure.jooq.tables.records.AirportsRecord
+import net.daester.david.flyawayjooqexample.infrastructure.jooq.Tables.*
+import net.daester.david.flyawayjooqexample.infrastructure.jooq.enums.AirportsScheduledService
+import net.daester.david.flyawayjooqexample.infrastructure.jooq.enums.AirportsType
+import net.daester.david.flyawayjooqexample.infrastructure.jooq.enums.VAirportDistancesTargetAirportScheduledService
+import net.daester.david.flyawayjooqexample.infrastructure.jooq.tables.interfaces.IAirports
 import net.daester.david.flywayjooqexample.domain.Airport
 import net.daester.david.flywayjooqexample.domain.AirportPosition
 import net.daester.david.flywayjooqexample.domain.AirportRepository
-import org.jooq.Configuration
 import org.jooq.DSLContext
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
 
 @Repository
-class JooqAirportRepository(private val jooq: DSLContext, configuration: Configuration) : AirportRepository, AirportsDao(configuration) {
+class JooqAirportRepository(private val jooq: DSLContext) : AirportRepository {
     val LOGGER = LoggerFactory.getLogger(JooqAirportRepository::class.java)
 
-    override fun getById(id: Int): Airport = findById(id).asDomain()
+    override fun getById(id: Int): Airport = jooq.selectFrom(AIRPORTS).where(AIRPORTS.ID.eq(id)).fetchOne().asDomain()
 
-    override fun getByIdent(ident: String): Airport = fetchOneByIdent(ident).asDomain()
+    override fun getByIdent(ident: String): Airport = jooq.selectFrom(AIRPORTS).where(AIRPORTS.IDENT.eq(ident)).fetchOne().asDomain()
 
-    override fun getAllAirports(): List<Airport> = findAll().map { it.asDomain() }
+    override fun getAllAirports(): List<Airport> = jooq.selectFrom(AIRPORTS).map { it.asDomain() }
 
     override fun getAllAirportsByType(type: String): List<Airport>
             =
             try {
-                fetchByType(AirportsType.valueOf(type)).map { it.asDomain() }
+                jooq.selectFrom(AIRPORTS).where(AIRPORTS.TYPE.eq(AirportsType.valueOf(type))).map { it.asDomain() }
             } catch (e: IllegalArgumentException) {
                 LOGGER.warn("Illegal argument: {}", e.message)
                 emptyList()
@@ -60,13 +55,7 @@ class JooqAirportRepository(private val jooq: DSLContext, configuration: Configu
                     ).map { it.asDomain() }
 
 
-    private fun AirportsRecord.asDomain() = Airport(id, ident, type.name, name, when (scheduledService) {
-        AirportsScheduledService.yes -> true
-        AirportsScheduledService.no -> false
-        else -> null
-    }, AirportPosition(longitudeDeg, latitudeDeg))
-
-    private fun Airports.asDomain() = Airport(id, ident, type.name, name, when (scheduledService) {
+    private fun IAirports.asDomain() = Airport(id, ident, type.name, name, when (scheduledService) {
         AirportsScheduledService.yes -> true
         AirportsScheduledService.no -> false
         else -> null
